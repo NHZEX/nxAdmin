75d4ddb746aa4557ab0e3d054ba6df4b1f514841
auooru <auooru@163.com>
2019-03-23 16:12:13 +0800

    添加更新程序[TEST]

---
+++ application/command/DeployUpdate.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: NHZEXG
+ * Date: 2019/3/23
+ * Time: 11:59
+ */
+declare(strict_types=1);
+
+namespace app\command;
+
+use basis\Util;
+use think\console\Command;
+use think\console\Input;
+use think\console\input\Option;
+use think\console\Output;
+use think\facade\App;
+
+class DeployUpdate extends Command
+{
+    public function configure()
+    {
+        $this
+            ->setName('deploy:update')
+            ->addOption('mode', 'm', Option::VALUE_OPTIONAL, '模式选择 [git]', 'git')
+            ->addOption('yes', 'y', Option::VALUE_NONE, '更新确认')
+            ->setDescription('执行系统更新 [TEST]');
+    }
+
+    /**
+     * @param Input  $input
+     * @param Output $output
+     * @return int
+     */
+    public function execute(Input $input, Output $output): int
+    {
+        $output->info('whoami: ' . Util::whoami());
+        $mode = $input->getOption('mode');
+
+        switch ($mode) {
+            case 'git':
+                $result = $this->gitUpdate();
+                break;
+            default:
+                $this->output->error("Invalid mode: {$mode}");
+                $result = 1;
+        }
+        return $result;
+    }
+
+    public function gitUpdate(): int
+    {
+        $yes = (bool)$this->input->getOption('yes');
+
+        $git_dir = App::getRootPath() . '.git';
+        if (!is_dir($git_dir)) {
+            $this->output->error("{$git_dir} does not exist");
+            return 1;
+        }
+
+        $commands = [
+            'echo $PWD',
+            'git status',
+            'git fetch',
+            $yes ? 'git pull' : null,
+            'git submodule sync',
+            'git submodule update',
+            $yes ? 'git submodule status' : null,
+        ];
+
+        foreach ($commands as $command) {
+            if (empty($command)) {
+                continue;
+            }
+            $this->output->warning("exec: {$command}");
+            // Run it
+            $opts = [];
+            exec($command, $opts, $return);
+            // Output
+            foreach ($opts as $opt) {
+                $this->output->writeln($opt);
+            }
+            if (0 !== $return) {
+                $this->output->error("exec fail: {$command}");
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
