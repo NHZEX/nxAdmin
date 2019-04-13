0970179f09227d5da0f2a716d6561e6917e833fd
auooru <auooru@163.com>
2019-04-13 11:05:18 +0800

    调整乐观锁代码架构

--- application/model/Base.php
+++ application/model/Base.php
@@ -8,11 +8,9 @@
 
 namespace app\model;
 
-use db\exception\ModelException;
 use db\Query as Query2;
 use db\traits\ModelUtil;
 use db\traits\OptimLock;
-use Exception;
 use facade\WebConv;
 use think\Model as ThinkModel;
 
@@ -67,124 +65,6 @@ abstract class Base extends ThinkModel
     protected $createBy = 'create_by';
     /** @var string 更新者UID */
     protected $updateBy = 'update_by';
-
-    /**
-     * @param mixed $where
-     * @return bool
-     * @throws Exception
-     */
-    protected function updateData($where)
-    {
-        // 获取必要数据 MOD
-        $is_optim_lock = $this->optimLock && isset($this[$this->optimLock]);
-        $optimLockValue = $this[$this->optimLock] ?? 0;
-
-        // 自动更新
-        $this->autoCompleteData($this->update);
-
-        // 事件回调
-        if (false === $this->trigger('before_update')) {
-            return false;
-        }
-
-        // 获取有更新的数据
-        $data = $this->getChangedData();
-        // 移除无效数据 MOD
-        foreach (array_diff(array_keys($data), $this->getTableFields()) as $k) {
-            unset($data[$k]);
-        }
-
-        if (empty($data)) {
-            // 关联更新
-            if (!empty($this->relationWrite)) {
-                $this->autoRelationUpdate();
-            }
-
-            return false;
-        } elseif ($this->autoWriteTimestamp && $this->updateTime && !isset($data[$this->updateTime])) {
-            // 自动写入更新时间
-            $data[$this->updateTime] = $this->autoWriteTimestamp($this->updateTime);
-
-            $this->data($this->updateTime, $data[$this->updateTime]);
-        }
-
-        // 自增锁版本 MOD
-        if ($is_optim_lock) {
-            $data[$this->optimLock] = $this->getData($this->optimLock) + 1;
-        }
-
-        // 尝试获取查询条件
-        if (empty($where) && !empty($tmpWhere = $this->getWhere())) {
-            $where = $tmpWhere;
-        }
-
-        // 检查允许字段
-        $allowFields = $this->checkAllowFields(array_merge($this->auto, $this->update));
-
-        // 保留主键数据
-        foreach ($this->getData() as $key => $val) {
-            if ($this->isPk($key)) {
-                $data[$key] = $val;
-            }
-        }
-
-        $pk    = $this->getPk();
-        $array = [];
-
-        foreach ((array) $pk as $key) {
-            if (isset($data[$key])) {
-                $array[] = [$key, '=', $data[$key]];
-                unset($data[$key]);
-            }
-        }
-
-        if (!empty($array)) {
-            $where = $array;
-        }
-
-        foreach ((array) $this->relationWrite as $name => $val) {
-            if (is_array($val)) {
-                foreach ($val as $key) {
-                    if (isset($data[$key])) {
-                        unset($data[$key]);
-                    }
-                }
-            }
-        }
-
-        // 模型更新
-        $db = $this->db(false);
-        $db->startTrans();
-
-        try {
-            $tmp = $db->where($where);
-            // 设置乐观锁条件 MOD
-            $is_optim_lock && $tmp->where($this->optimLock, '=', $optimLockValue);
-            $tmp->strict(false)
-                ->field($allowFields)
-                ->update($data);
-
-            // 检测是否更新成功 MOD
-            if ($is_optim_lock && $this->getConnection()->getNumRows() === 0) {
-                throw new ModelException('The object being updated is outdated.', CODE_MODEL_OPTIMISTIC_LOCK);
-            }
-
-            // 关联更新
-            if (!empty($this->relationWrite)) {
-                $this->autoRelationUpdate();
-            }
-
-            $db->commit();
-
-            // 更新回调
-            $this->trigger('after_update');
-
-            return true;
-        } catch (Exception $e) {
-            $db->rollback();
-            throw $e;
-        }
-    }
 
     // 全局模型初始化追加
     protected function initialize()
