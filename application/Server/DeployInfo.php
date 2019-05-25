d5f2f382c05b1f4a25cba95e2e7bc300c92fc801
auooru <auooru@163.com>
2019-05-25 22:43:08 +0800

    改进部署文件生成

--- application/Server/DeployInfo.php
+++ application/Server/DeployInfo.php
@@ -31,14 +31,24 @@ class DeployInfo
      */
     public static function init()
     {
+        $app = App::getInstance();
+
+        if (
+            $app->env->get('DEPLOY_SECURITY_SALT', false)
+            && $app->env->get('DEPLOY_ROOT_PATH_SIGN', false)
+            && $app->env->get('DEPLOY_MIXING_PREFIX', false)
+        ) {
+            return [];
+        }
+
         $security_salt = get_rand_str(32);
-        $root_path_sign = dechex(crc32(App::getInstance()->getRootPath() . 'dir'));
+        $root_path_sign = dechex(crc32($app->getRootPath() . 'dir'));
         $mixing_prefix = $root_path_sign . '_' . dechex(crc32($security_salt));
 
         $env_contents = [
-            'security_salt' => $security_salt,
-            'root_path_sign' => $root_path_sign,
-            'mixing_prefix' => $mixing_prefix,
+            'DEPLOY_SECURITY_SALT' => $security_salt,
+            'DEPLOY_ROOT_PATH_SIGN' => $root_path_sign,
+            'DEPLOY_MIXING_PREFIX' => $mixing_prefix,
         ];
 
         return $env_contents;
@@ -57,7 +67,7 @@ class DeployInfo
         }
 
         if (0 === strpos($name, 'get')) {
-            $value = App::getInstance()->env->get(self::ITEM_NAME . '_' . Util::toSnakeCase(substr($name, 3)));
+            $value = App::getInstance()->env->get('DEPLOY_' . Util::toSnakeCase(substr($name, 3)));
             return self::$CAHCE[$name] = $value;
         }
 
