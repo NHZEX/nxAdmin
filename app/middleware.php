9f5788d4d2f173807802952e29ac52adf35a6c08
Johnson <821312534@qq.com>
2019-06-14 14:29:56 +0800

    tp升级   - ExceptionHandle添加 不记录信息（日志）的异常类列表  - 删除无用配置

--- app/middleware.php
+++ app/middleware.php
@@ -6,22 +6,22 @@
  */
 
 use app\Middleware\Authorize;
-use app\Middleware\CrossSiteRequest;
 use app\Middleware\Exception;
 use app\Middleware\Validate;
+use think\middleware\SessionInit;
 
 return [
 /*    CrossSiteRequest::class,*/
-    \think\middleware\SessionInit::class,
+    SessionInit::class,
     Authorize::class,
     Validate::class,
     Exception::class,
     // 全局请求缓存
-    // 'think\middleware\CheckRequestCache',
+    // \think\middleware\CheckRequestCache::class,
     // 多语言加载
-    // 'think\middleware\LoadLangPack',
+    // \think\middleware\LoadLangPack::class,
     // Session初始化
-    // 'think\middleware\SessionInit',
+    // \think\middleware\SessionInit::class,
     // 页面Trace调试
-    // 'think\middleware\TraceDebug',
+    // \think\middleware\TraceDebug::class,
 ];
