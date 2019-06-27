59ea5ffc0f24538278741d36c163c490c5dafc6b
auooru <auooru@163.com>
2019-06-27 17:53:03 +0800

    修复调试助手中间件

--- app/Service/DebugHelper/DebugHelperService.php
+++ app/Service/DebugHelper/DebugHelperService.php
@@ -21,7 +21,6 @@ class DebugHelperService extends Service
                 ini_set('xdebug.var_display_max_children', '256');
                 ini_set('xdebug.var_display_max_data', '1024');
             }
-
             $this->app->middleware->add(DebugRequestInfo::class);
         }
     }
