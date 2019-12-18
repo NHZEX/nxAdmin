4cfe48bf9e3e6ba71243782284935af26c733c28
auooru <auooru@163.com>
2019-12-18 15:43:26 +0800

    添加 Model::subQuery 方法

--- app/Model/Base.php
+++ app/Model/Base.php
@@ -10,6 +10,7 @@ namespace app\Model;
 
 use Closure;
 use DateTime;
+use think\db\Query;
 use think\db\Raw;
 use think\Model as ThinkModel;
 use think\model\Collection;
@@ -80,5 +81,24 @@ abstract class Base extends ThinkModel
     public function isDisableAccessControl(): bool
     {
         return PHP_SAPI === 'cli' && defined('DISABLE_ACCESS_CONTROL');
+    }
+
+    /**
+     * 构建子查询
+     * @param Closure $closure
+     * @param string|null $field
+     * @return Closure
+     */
+    public static function subQuery(Closure $closure, ?string $field)
+    {
+        return function (Query $query) use ($closure, $field) {
+
+            $query->table((new static())->getTable());
+            $closure($query);
+
+            if (!empty($field)) {
+                $query->field($field);
+            }
+        };
     }
 }
