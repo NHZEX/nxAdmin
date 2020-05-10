15c0a4886b219af09bd2dafd520d50ed57f88b28
auooru <auooru@outlook.com>
2020-05-10 23:57:01 +0800

    update: 完善远程日志头

--- config/log.php
+++ config/log.php
@@ -5,18 +5,18 @@
 // +----------------------------------------------------------------------
 
 use think\App;
-use think\Request;
 
 // 格式日志头
-$formatHead = function ($uir, App $app, Request $request) {
-    $uri_method  = " [{$request->method()}]";
+$formatHead = function ($uir, App $app) {
+    $method      = $app->exists('request') ? $app->request->method() : 'NULL';
+    $method      = " [$method]";
     $runtime     = round(microtime(true) - $app->getBeginTime(), 10);
     $time_str    = ' [运行时间：' . number_format($runtime, 6) . 's]';
     $memory_use  = number_format((memory_get_usage() - $app->getBeginMem()) / 1024, 2);
     $memory_peak = number_format(memory_get_peak_usage() / 1024, 2);
     $memory_str  = ' [内存消耗：' . $memory_use . 'kb，峰值：' . $memory_peak . 'kb]';
     $file_load   = ' [文件加载：' . count(get_included_files()) . ']';
-    return $uir . $uri_method . $time_str . $memory_str . $file_load;
+    return $uir . $method . $time_str . $memory_str . $file_load;
 };
 
 return [
