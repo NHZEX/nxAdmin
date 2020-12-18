557846ab411d97f44987415f40ba239d6b82b599
auooru <auooru@163.com>
2020-12-18 11:37:44 +0800

    update: validate 增强 [2]

--- app/Validate/Base.php
+++ app/Validate/Base.php
@@ -2,8 +2,12 @@
 
 namespace app\Validate;
 
+use think\Validate;
 use Zxin\Think\Validate\ValidateBase;
+use function array_map;
 use function filter_var;
+use function is_array;
+use function join;
 use const FILTER_VALIDATE_INT;
 
 abstract class Base extends ValidateBase
@@ -15,33 +19,71 @@ abstract class Base extends ValidateBase
      */
     protected function isPositiveInteger($value)
     {
-        return $this->isInteger($value, '+');
+        return $this->isNumber($value, 'int+');
     }
 
     /**
      * 判断是否为整数
-     * @param $value
-     * @param $params
+     * @param      $value
+     * @param      $params
      * @return bool|string
      */
-    protected function isInteger($value, $params)
+    protected function isInteger($value, ?string $params)
     {
-        if (($result = filter_var($value, FILTER_VALIDATE_INT)) === false) {
-            return ':attribute不是一个整数';
+        return $this->isNumber($value, "int{$params}");
+    }
+
+    /**
+     * 判断是否为数值
+     * @param      $value
+     * @param      $params
+     * @return bool|string
+     */
+    protected function isNumber($value, ?string $params)
+    {
+        $isInt = str_contains($params, 'int');
+        if (($result = filter_var($value, $isInt ? FILTER_VALIDATE_INT : FILTER_VALIDATE_FLOAT)) === false) {
+            if ($isInt) {
+                return ':attribute必须是一个整数';
+            } else {
+                return ':attribute必须是一个数值';
+            }
         }
         if (empty($params)) {
             return true;
         }
         $positive = str_contains($params, '+');
         $negative  = str_contains($params, '-');
-        if ($positive && $result >= 0) {
+        if ($positive && $negative) {
+            return true;
+        } elseif ($positive && $result >= 0) {
             return true;
         } elseif (!$negative) {
-            return ':attribute不是一个正数';
+            return ':attribute必须是一个正数';
         } elseif ($result < 0) {
             return true;
         } else {
-            return ':attribute不是一个负数';
+            return ':attribute必须是一个负数';
         }
+    }
+
+    /**
+     * @param Validate $valid
+     * @param array    $value
+     * @return true|string
+     */
+    public static function subValidateCall(Validate $valid, array $value)
+    {
+        if ($valid->check($value) === false) {
+            $error = $valid->getError();
+            if (is_array($error)) {
+                return join(', ', array_map(function ($str) {
+                    return ":attribute->{$str}";
+                }, $error));
+            } else {
+                return ":attribute->{$error}";
+            }
+        }
+        return true;
     }
 }
