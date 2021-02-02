79415c9cb3fc7dc2a1cb7952e3ac25eeac89ccf7
auooru <auooru@163.com>
2021-02-02 17:20:07 +0800

    update: 添加兼容性代码

---
+++ app/Service/Auth/AuthManager.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace app\Service\Auth;
+
+/**
+ * Class AuthManager
+ * @package app\Service\Auth
+ * @deprecated use \app\Service\Auth\AuthHelper
+ */
+class AuthManager extends AuthHelper
+{
+}
