7a4ddebaa91295ecd86e78b12c4f01bad3887923
auooru <auooru@outlook.com>
2021-07-21 02:11:51 +0800

    update: 完善模型事件处理

--- extend/Tp/Model/Event.php
+++ extend/Tp/Model/Event.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Tp\Model;
 
 use function call_user_func;
+use function end;
+use function is_null;
 
 class Event
 {
@@ -25,14 +27,21 @@ class Event
      * 触发事件
      * @param string $event  事件名
      * @param mixed  $params 传入参数
+     * @param bool   $once   只获取一个有效返回值
      */
-    public function trigger(string $event, $params = null)
+    public function trigger(string $event, $params = null, bool $once = false)
     {
+        $result = [];
         if (isset($this->event[$event])) {
-            foreach ($this->event[$event] as $callback) {
-                call_user_func($callback, $params);
+            foreach ($this->event[$event] as $key => $callback) {
+                $result[$key] = call_user_func($callback, $params);
+                if (false === $result[$key] || (!is_null($result[$key]) && $once)) {
+                    break;
+                }
             }
         }
+
+        return $once ? end($result) : $result;
     }
 
     /**
