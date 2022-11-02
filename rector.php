dd18714a307ab596308c7c8f75cd7b56f0315456
auooru <auooru@outlook.com>
2022-11-03 00:56:48 +0800

    update: rector.php

--- rector.php
+++ rector.php
@@ -2,37 +2,33 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
 use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
 use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
 use Rector\Set\ValueObject\LevelSetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    // get parameters
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
         __DIR__ . '/app',
         __DIR__ . '/config',
         __DIR__ . '/extend',
         __DIR__ . '/route',
     ]);
-
-    $parameters->set(Option::BOOTSTRAP_FILES, [
+    $rectorConfig->bootstrapFiles([
         __DIR__ . '/app/common.php',
         __DIR__ . '/vendor/autoload.php',
     ]);
 
-    $parameters->set(Option::SKIP, [
+    $rectorConfig->skip([
         __DIR__ . '/app/auth_storage.php',
         __DIR__ . '/app/validate_storage.php',
         JsonThrowOnErrorRector::class,
         AddLiteralSeparatorToNumberRector::class,
     ]);
 
-     // $parameters->set(Option::AUTO_IMPORT_NAMES, true);
-     // $parameters->set(Option::IMPORT_SHORT_CLASSES, false);
 
-    // Define what rule sets will be applied
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_74);
+    // define sets of rules
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_74,
+    ]);
 };
