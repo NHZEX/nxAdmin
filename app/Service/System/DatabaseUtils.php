368961f84d0dc301515273d39dc4c4f973396323
auooru <auooru@outlook.com>
2023-03-30 01:07:23 +0800

    update: 完善查询错误处理

--- app/Service/System/DatabaseUtils.php
+++ app/Service/System/DatabaseUtils.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 namespace app\Service\System;
+use think\db\exception\DbException;
 use function Zxin\Util\format_byte;
 
 class DatabaseUtils
@@ -14,38 +15,45 @@ class DatabaseUtils
         $output = [];
 
         foreach ($connections as $name => $config) {
-            $connection = $db->connect($name);
-            $list = $connection
-                ->table('information_schema.tables')
-                ->field([
-                    'table_schema', 'table_name', 'auto_increment','table_rows',
-                    'avg_row_length', 'data_length', 'index_length', 'data_free',
-                    'create_time', 'update_time', 'table_comment',
-                ])
-                ->whereRaw('table_schema=SCHEMA()')
-                ->order('table_name', 'asc')
-                ->select();
+            try {
+                $connection = $db->connect($name);
+                $list = $connection
+                    ->table('information_schema.tables')
+                    ->field([
+                        'table_schema', 'table_name', 'auto_increment','table_rows',
+                        'avg_row_length', 'data_length', 'index_length', 'data_free',
+                        'create_time', 'update_time', 'table_comment',
+                    ])
+                    ->whereRaw('table_schema=SCHEMA()')
+                    ->order('table_name', 'asc')
+                    ->select();
 
-            $list = $list->map(function ($item) {
-                $output = [];
-                foreach ($item as $key => $value) {
-                    $output[\strtolower($key)] = $value;
-                }
+                $list = $list->map(function ($item) {
+                    $output = [];
+                    foreach ($item as $key => $value) {
+                        $output[\strtolower($key)] = $value;
+                    }
 
-                $output['human'] = [
-                    'avg_row_size' => format_byte($output['avg_row_length'], 3),
-                    'total_size' => format_byte($output['data_length'] + $output['index_length'], 3),
-                    'data_size' => format_byte($output['data_length'], 3),
-                    'index_size' => format_byte($output['index_length'], 3),
-                    'data_free_size' => format_byte($output['data_free'], 3),
-                ];
+                    $output['human'] = [
+                        'avg_row_size' => format_byte($output['avg_row_length'], 3),
+                        'total_size' => format_byte($output['data_length'] + $output['index_length'], 3),
+                        'data_size' => format_byte($output['data_length'], 3),
+                        'index_size' => format_byte($output['index_length'], 3),
+                        'data_free_size' => format_byte($output['data_free'], 3),
+                    ];
 
-                return $output;
-            });
+                    return $output;
+                });
+            } catch (DbException $e) {
+                $list = [];
+                $message = $e->getMessage();
+                \log_warning($e);
+            }
 
             $output[] = [
                 'name' => $name,
                 'tables' => $list,
+                'message' => $message ?? null,
             ];
         }
 
