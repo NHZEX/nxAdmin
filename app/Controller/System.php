21b9f88b81ad550803280ed65de6460d433ccb9e
auooru <auooru@outlook.com>
2023-09-08 13:59:17 +0800

    update: 分离环境信息获取

--- app/Controller/System.php
+++ app/Controller/System.php
@@ -4,6 +4,7 @@ namespace app\Controller;
 
 use app\Logic\SystemLogic;
 use app\Service\System\DatabaseUtils;
+use app\Utils;
 use think\middleware\Throttle;
 use think\Response;
 use Util\Reply;
@@ -12,11 +13,6 @@ use Zxin\Think\Auth\Annotation\Auth;
 use Zxin\Think\Route\Annotation\Group;
 use Zxin\Think\Route\Annotation\Middleware;
 use Zxin\Think\Route\Annotation\Route;
-use function ini_get;
-use function php_uname;
-use function realpath_cache_size;
-use const PHP_SAPI;
-use const PHP_VERSION;
 
 #[Group('system')]
 class System extends ApiBase
@@ -37,20 +33,7 @@ class System extends ApiBase
     #[Route(method: 'GET')]
     public function sysinfo(): Response
     {
-        return Reply::success([
-            'sys_version' => ['服务器系统', php_uname()],
-            'server_software' => ['执行环境', $_SERVER['SERVER_SOFTWARE']],
-            'php_sapi' => ['PHP接口类型', PHP_SAPI],
-            'tp_version' => ['ThinkPHP 版本', $this->app->version()],
-            'php_version' => ['PHP版本', PHP_VERSION],
-            'db_version' => ['数据库版本', db_version(null, true)],
-            'memory_limit' => ['内存限制', ini_get('memory_limit')],
-            'max_execution_time' => ['最长执行时间', ini_get('max_execution_time')],
-            'upload_max_filesize' => ['上传限制', ini_get('upload_max_filesize')],
-            'post_max_size' => ['POST限制', ini_get('post_max_size')],
-            'realpath_cache_size' => ['路径缓存', realpath_cache_size()],
-            'preload_statistics' => ['预加载', preload_statistics()],
-        ]);
+        return Reply::success(Utils::getEnvInfo());
     }
 
     #[Auth('admin')]
--- app/Utils.php
+++ app/Utils.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace app;
 
+use Composer\InstalledVersions;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LogLevel;
 use Symfony\Component\Console\Logger\ConsoleLogger;
@@ -52,5 +53,24 @@ class Utils
         self::$loggerCache[$output] = $logger;
 
         return $logger;
+    }
+
+    public static function getEnvInfo(): array
+    {
+        return [
+            'sys_version' => ['服务器系统', \php_uname()],
+            'server_software' => ['执行环境', $_SERVER['SERVER_SOFTWARE']],
+            'php_sapi' => ['PHP接口类型', PHP_SAPI],
+            'tp_version' => ['ThinkPHP 版本', InstalledVersions::getPrettyVersion("topthink/framework")],
+            'orm_version' => ['ThinkORM 版本', InstalledVersions::getPrettyVersion("topthink/think-orm")],
+            'php_version' => ['PHP版本', PHP_VERSION],
+            'db_version' => ['数据库版本', \db_version(null, true)],
+            'memory_limit' => ['内存限制', \ini_get('memory_limit')],
+            'max_execution_time' => ['最长执行时间', \ini_get('max_execution_time')],
+            'upload_max_filesize' => ['上传限制', \ini_get('upload_max_filesize')],
+            'post_max_size' => ['POST限制', \ini_get('post_max_size')],
+            'realpath_cache_size' => ['路径缓存', \realpath_cache_size()],
+            'preload_statistics' => ['预加载', \preload_statistics()],
+        ];
     }
 }
