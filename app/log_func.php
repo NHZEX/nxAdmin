c7158a5f01decd1671753f6237eca229443642fe
auooru <auooru@outlook.com>
2024-06-11 22:10:48 +0800

    update: 改进日志打印助手

--- app/log_func.php
+++ app/log_func.php
@@ -2,45 +2,53 @@
 
 function log_emergency($message, array $context = [])
 {
-    app()->log->log('emergency', $message, $context);
+    __log_record_ex('emergency', $message, $context);
 }
 
 function log_alert($message, array $context = [])
 {
-    app()->log->log('alert', $message, $context);
+    __log_record_ex('alert', $message, $context);
 }
 
 function log_critical($message, array $context = [])
 {
-    app()->log->log('critical', $message, $context);
+    __log_record_ex('critical', $message, $context);
 }
 
 function log_error($message, array $context = [])
 {
-    app()->log->log('error', $message, $context);
+    __log_record_ex('error', $message, $context);
 }
 
 function log_warning($message, array $context = [])
 {
-    app()->log->log('warning', $message, $context);
+    __log_record_ex('warning', $message, $context);
 }
 
 function log_notice($message, array $context = [])
 {
-    app()->log->log('notice', $message, $context);
+    __log_record_ex('notice', $message, $context);
 }
 
 function log_info($message, array $context = [])
 {
-    app()->log->log('info', $message, $context);
+    __log_record_ex('info', $message, $context);
 }
 
 function log_debug($message, array $context = [])
 {
-    app()->log->log('debug', $message, $context);
+    __log_record_ex('debug', $message, $context);
 }
 
 function log_sql($message, array $context = [])
 {
-    app()->log->log('sql', $message, $context);
+    __log_record_ex('sql', $message, $context);
+}
+
+function __log_record_ex(string $level, $message, array $context)
+{
+    if (is_array($message) || is_object($message)) {
+        $message = var_export($message, true);
+    }
+    app()->log->log($level, $message, $context);
 }
