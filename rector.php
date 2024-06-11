ef344985fe5df0a189e0d2ceaf07ccccc983a3df
auooru <auooru@outlook.com>
2024-06-11 23:39:39 +0800

    update: rector.php

--- rector.php
+++ rector.php
@@ -5,34 +5,28 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
 use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
-use Rector\Set\ValueObject\LevelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/app',
         __DIR__ . '/config',
         __DIR__ . '/extend',
         __DIR__ . '/route',
-    ]);
-    $rectorConfig->bootstrapFiles([
+    ])
+    ->withBootstrapFiles([
         __DIR__ . '/app/common.php',
         __DIR__ . '/vendor/autoload.php',
-    ]);
-
-    $rectorConfig->skip([
+    ])
+    ->withSkip([
         __DIR__ . '/app/auth_storage.php',
         __DIR__ . '/app/route_storage.dump.php',
         __DIR__ . '/app/validate_storage.php',
         JsonThrowOnErrorRector::class,
         AddLiteralSeparatorToNumberRector::class,
+    ])
+    ->withPreparedSets(deadCode: true)
+    ->withImportNames(importShortClasses: false)
+    ->withPhpSets()
+    ->withPHPStanConfigs([
+        __DIR__ . '/phpstan.neon',
     ]);
-
-    $rectorConfig->rules([]);
-
-    // define sets of rules
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_74,
-    ]);
-
-    $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
-};
