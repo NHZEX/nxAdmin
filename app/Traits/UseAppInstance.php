37076aadb3f2b87a6a9c6cb67094d2eabcfe6d69
auooru <auooru@outlook.com>
2024-06-11 22:44:38 +0800

    add: UseAppInstance.php

---
+++ app/Traits/UseAppInstance.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace app\Traits;
+
+use think\App;
+
+trait UseAppInstance
+{
+    public static function instance(): static
+    {
+        return App::getInstance()->make(static::class);
+    }
+}
