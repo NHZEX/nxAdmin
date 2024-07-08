5fe1b63547cf42188d9e844e00f41a3c89bc6fda
auooru <auooru@outlook.com>
2024-07-08 16:43:50 +0800

    update: 添加调试辅助工具

---
+++ app/Service/DebugHelper/DebugHelper.php
@@ -0,0 +1,133 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\Service\DebugHelper;
+
+use app\Service\Context;
+use ArrayObject;
+use stdClass;
+use think\App;
+use Zxin\Think\Redis\RedisManager;
+use function Zxin\Util\format_byte;
+
+class DebugHelper
+{
+    private static array $timeList = [];
+
+    public static function startTime(string $name): void
+    {
+        $obj = new stdClass();
+        $obj->time = microtime(true);
+        $obj->memory = memory_get_usage();
+        $obj->peakMemory = memory_get_peak_usage();
+        self::$timeList[$name] = $obj;
+    }
+
+    public static function endTime(string $name): string
+    {
+        $obj = self::$timeList[$name] ?? null;
+        if (empty($obj)) {
+            return "{$name} not ready";
+        }
+        unset(self::$timeList[$name]);
+        $endTime = microtime(true);
+
+        $endMemory = memory_get_usage();
+        $endPeakMemory = memory_get_peak_usage();
+
+        return sprintf(
+            '[%d]%s, time: %.3f, mem: %s(%s)',
+            getmypid(),
+            $name,
+            $endTime - $obj->time,
+            format_byte($endMemory - $obj->memory, 3),
+            format_byte($endPeakMemory - $obj->peakMemory, 3),
+        );
+    }
+
+    public static function endTimeWithRecord(string $name, ?string $note = null): string
+    {
+        $obj = self::$timeList[$name] ?? null;
+        if (empty($obj)) {
+            return "{$name} not ready";
+        }
+        unset(self::$timeList[$name]);
+        $endTime = microtime(true);
+
+        $duration = sprintf('%.3fs', $endTime - $obj->time);
+
+        self::recordDuration($name, $duration, $note);
+
+        $endMemory = memory_get_usage();
+        $endPeakMemory = memory_get_peak_usage();
+
+        $message = sprintf(
+            '[%d]%s => time: %s; mem: %s(%s)%s',
+            getmypid(),
+            $name,
+            $duration,
+            format_byte($endMemory - $obj->memory, 3),
+            format_byte($endPeakMemory - $obj->peakMemory, 3),
+            $note ? ", {$note}" : '',
+        );
+
+        log_debug($message);
+
+        return $message;
+    }
+
+    public static function recordDuration(string $name, string $message, ?string $note = null): void
+    {
+        if (!Context::hasData('__DEBUG_TIME_LOGS')) {
+            $ctx = new ArrayObject(flags: ArrayObject::ARRAY_AS_PROPS);
+            Context::setData('__DEBUG_TIME_LOGS', $ctx);
+        } else {
+            $ctx = Context::getData('__DEBUG_TIME_LOGS');
+        }
+
+        $ctx->{$name} = $message.($note ? ", {$note}" : '');
+    }
+
+    public static function getTimerRecord(): string
+    {
+        /** @var ArrayObject|null $debugTimes */
+        $debugTimes = Context::getData('__DEBUG_TIME_LOGS');
+
+        if (null === $debugTimes) {
+            return '';
+        }
+
+        $strArr = [];
+        foreach ($debugTimes as $key => $message) {
+            $strArr[] = "{$key}=\"{$message}\"";
+        }
+
+        return implode('; ', $strArr);
+    }
+
+    public static function refreshRequestFrequency(?string $actionName, int $hour = 24): void
+    {
+        if (null === $actionName) {
+            $request = App::getInstance()->request;
+
+            $actionName = str_replace('/', '_', trim($request->baseUrl(), '/'));
+        }
+
+        $target = "request_rate:{$actionName}";
+
+        if (Context::hasData($target)) {
+            return;
+        }
+
+        $time = date('ymdHi');
+        $key = "{$target}:{$time}";
+        Context::setData($target, $time);
+
+        $redis = RedisManager::connection();
+        $val = $redis->incr($key);
+        if (1 === $val) {
+            $redis->expire($key, 3600 * $hour);
+        }
+    }
+}
