e89b9be46d69984380129190390da94a6d501130
auooru <auooru@163.com>
2025-05-15 23:07:07 +0800

    update: 优化调试信息中间件的注册优先级

--- app/Service/DebugHelper/DebugHelperService.php
+++ app/Service/DebugHelper/DebugHelperService.php
@@ -22,7 +22,7 @@ class DebugHelperService extends Service
                 ini_set('xdebug.var_display_max_children', '256');
                 ini_set('xdebug.var_display_max_data', '1024');
             }
-            $this->app->middleware->add(DebugRequestInfo::class, 'route');
+            $this->app->middleware->unshift(DebugRequestInfo::class, 'route');
         }
     }
 
