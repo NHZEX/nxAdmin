12f2c6bc2c8bb47e9c7fb17b85aca90822679e45
auooru <auooru@163.com>
2025-05-15 17:14:11 +0800

    update: 解决 8.1 新版本的日志兼容性

--- extend/Tp/Log/Channel.php
+++ extend/Tp/Log/Channel.php
@@ -4,15 +4,37 @@ declare(strict_types=1);
 
 namespace Tp\Log;
 
+use Composer\InstalledVersions;
 use think\event\LogRecord;
 use function strtr;
 
 class Channel extends \think\log\Channel
 {
+    private static ?bool $newImplement = null;
+
+    private static function isNewImplement(): bool
+    {
+        if (null !== self::$newImplement) {
+            return self::$newImplement;
+        }
+
+        $version = ltrim(InstalledVersions::getPrettyVersion('topthink/framework'), 'v');
+        if (preg_match('~(\d+\.?)+~', $version)) {
+            $newImplement = (bool) version_compare($version, '8.1.2', '>');
+        } else {
+            $newImplement = !class_exists('\think\log\driver\Socket');
+        }
+        return self::$newImplement = $newImplement;
+    }
+
     public function record($msg, string $type = 'info', array $context = [], bool $lazy = true)
     {
         if ($this->close || (!empty($this->allow) && !\in_array($type, $this->allow))) {
             return $this;
+        }
+
+        if ($msg instanceof \Stringable) {
+            $msg = (string) $msg;
         }
 
         if (\is_string($msg) && !empty($context)) {
@@ -31,7 +53,11 @@ class Channel extends \think\log\Channel
         }
 
         if (!empty($msg) || 0 === $msg) {
-            $this->log[$type][] = $msg;
+            if (self::isNewImplement()) {
+                $this->log[] = [$type, $msg];
+            } else {
+                $this->log[$type][] = $msg;
+            }
             if ($this->event) {
                 $this->event->trigger(new LogRecord($type, $msg));
             }
